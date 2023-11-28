fd6f27d99181b9d145298069fa57ca2a4b05ff4a
elhaj-lachgar <143610909+elhaj-lachgar@users.noreply.github.com>
2023-11-28 14:54:44 +0100

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require ('express');
+const dotenv = require ('dotenv');
 
 
 const app = express();
+// configrition
+dotenv.config({path:"config.env"});
 
 
 
-app.listen()
+
+
+
+const server = app.listen( process.env.PORT , (message) => {
+    if ( process.env.NODE_ENV == "dev" )
+      console.log('port is opend ' , process.env.PORT );
+})
+
+
+process.on('unhandledRejection' , (err) => {
+    console.log(err);
+    server.close(() => {
+        console.log('server Shut Down')
+        process.exit(1);
+    })
+})
